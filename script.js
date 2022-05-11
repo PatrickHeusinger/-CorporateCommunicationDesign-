9e517d56bfbc73c723e555e1f0d99f2b5ba8cc1e
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2022-05-11 18:46:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function show() {
+    document.getElementById('hover').classList.add('whiteTextHover');
+    document.getElementById('hover').classList.add('blueBackgroundHover');
+}
+
+function hide() {
+    document.getElementById('hover').classList.remove('whiteTextHover');
+    document.getElementById('hover').classList.remove('blueBackgroundHover');
+}
